97feaef673f80c594a99f30b083f41a2451e29f5
Pedro <www.psro@gmail.com>
2020-06-07 21:21:56 -0300

    Refactoring: move readf function then remove the remaining bd file

--- brainduck.c
+++ brainduck.c
@@ -8,6 +8,30 @@
 #include "bdmemory.c"
 #include "bdloops.c"
 
+/** helper functions **/
+char *readf(char *filename) {
+	/** copy the content of a text file to a c-string**/
+	FILE *source = fopen(filename, "r");
+	char *program = malloc(1);
+	int len = 1;
+	char c;
+
+	/** read till the end **/
+	while ((c = fgetc(source)) != EOF) {
+		/** copy program to string */
+		program[len-1] = c;
+		program = realloc(program, ++len);
+			
+	}
+
+	/** end of string **/
+	program = realloc(program, len+1);
+	program[len] = '\0';
+	fclose(source);
+
+	return program;
+}
+
 
 /** executing brainf*ck **/
 void execBrainfuck(Program *prog, Memory *mem) {
