82daa6edf7af1a4efa1acc08267435bdabf7050b
Pedro <www.psro@gmail.com>
2020-08-16 20:33:04 +0100

    lib : interpreter function moved to bd_interpreter.c

--- src/lib/brainduck.c
+++ src/lib/brainduck.c
@@ -6,14 +6,23 @@
 
 #include <stdlib.h>
 
-#define MEM_SIZE 30000
 
-// BRAINDUCK LOOPS
+// TYPES
+
 typedef struct {
 	int length;
 	int *positions;
 } bdl;
 
+
+// FUNCTIONS
+
+void bdl_init(bdl *lp);
+void bdl_add(bdl *lp, int pc); 
+void bdl_remove(bdl *lp, unsigned char cell, int *pc);
+void bdl_free(bdl *lp); 
+int  bdl_last(bdl *lp);
+  
 void bdl_init(bdl *lp)
 {
 	// set length zero by default
@@ -64,69 +73,4 @@ void bdl_free(bdl *lp)
 
 // INTERPRETER
 
-void bd_meminfo(unsigned char *memory, int rightmost_cell, int pointer){
-   putchar('\n');
-   puts(BD_LOGO);
-   puts("Address  Value  Pointer");
-   for (int i = 0; i <= rightmost_cell; i++) {
-     bool is_pointer = (i == pointer);
-     printf("%7d  %5d  %s\n", i, memory[i], is_pointer? "  <-" : " ");
-   }
-}
 
-
-
-void bd_execute(char *program, bool debug){
-	// loops
-  bdl loops;
-	bdl_init(&loops);
-  
-  // memory
-	unsigned char * memory = malloc(sizeof (char));
-  *memory = 0;
-	unsigned int pointer = 0;
-  int rightmost_cell  = 0;
-
-	for (int i = 0; i < strlen(program); i++) {
-		switch (program[i]) {
-      // add or sub operation
-			case '+': memory[pointer]++; break;
-			case '-': memory[pointer]--; break;
-      
-      // memory operations
-			case '>': {
-        if (pointer+1 > rightmost_cell) {
-          memory = realloc(memory, pointer+1);
-          rightmost_cell++;
-          memory[rightmost_cell] = 0;
-        }
-        pointer++;
-        break; 
-      }
-
-			case '<': {
-        if (pointer) pointer--;
-        break;
-      }
-      
-      // flow control
-			case '[': bdl_add(&loops, i); break;
-      case ']': bdl_remove(&loops, memory[pointer], &i); break;
-
-			// i/o
-      case '.': putchar(memory[pointer]); break;
-			case ',': memory[pointer] = getchar();
-		}
-
-  }
-
-  // free memory used by loops
-	bdl_free(&loops);
-  
-  // debug memory usage at final of program
-  if (debug) { 
-    bd_meminfo(memory, rightmost_cell, pointer);
-  }
-
-  free(memory);
-}
